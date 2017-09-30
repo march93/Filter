056d8d4e2b17ea5dd0eb5ffac712e1b532c25650
Gabriel Lim <gabriel_lim@sfu.ca>
2017-09-30 16:47:28 -0700

    Added tsunami networking

--- app/src/main/java/com/android/filter/MainActivity.java
+++ app/src/main/java/com/android/filter/MainActivity.java
@@ -1,7 +1,11 @@
 package com.android.filter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setOnClick(R.id.tsunami, TsunamiActivity.class);
+    }
+
+    private void setOnClick(int resourceID, final Class nextActivity) {
+        TextView textView = (TextView) findViewById(resourceID);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent explicitIntent = new Intent(MainActivity.this, nextActivity);
+                startActivity(explicitIntent);
+                Log.v("ACTIVITY LAUNCHED", String.format("Launched %s", nextActivity));
+            }
+        });
     }
 }
