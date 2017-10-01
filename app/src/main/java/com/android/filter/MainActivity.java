30d6a648cbbfa5e7fa00719b42c8d30d5969ad10
Gabriel Lim <gabriel_lim@sfu.ca>
2017-10-01 09:05:09 -0700

    Fixed merge conflicts

--- app/src/main/java/com/android/filter/MainActivity.java
+++ app/src/main/java/com/android/filter/MainActivity.java
@@ -11,19 +11,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
     Button button;
+
+    Map<Integer, Class> ACTIVITY_MAP = new HashMap<Integer, Class>() {{
+        put(R.id.tsunami, TsunamiActivity.class);
+        put(R.id.place, UniversityActivity.class);
+    }};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setupSearchLocation();
+        setupButtonOnClick();
+    }
+
+    private void setupSearchLocation() {
         final EditText editText = (EditText) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.searchLocation);
         button.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                // Code here executes on main thread after user presses button
                 String result = editText.getText().toString();
 
                 if (result.length() < 1) {
@@ -40,6 +53,27 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     result = result.toLowerCase();
                 }
+            }
+        });
+    }
+
+    private void setupButtonOnClick() {
+        for (Map.Entry<Integer, Class> entry : ACTIVITY_MAP.entrySet()) {
+            Integer resourceID = entry.getKey();
+            Class activityClass = entry.getValue();
+
+            setOnClick(resourceID, activityClass);
+        }
+    }
+
+    private void setOnClick(Integer resourceID, final Class nextActivity) {
+        TextView textView = (TextView) findViewById(resourceID);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent explicitIntent = new Intent(MainActivity.this, nextActivity);
+                startActivity(explicitIntent);
+                Log.v("ACTIVITY LAUNCHED", String.format("Launched %s", nextActivity));
             }
         });
     }
