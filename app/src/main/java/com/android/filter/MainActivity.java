68cfeabdfb1965efaee804aba88d6f9515f2fe5f
Marc Ho <marc.ho380@gmail.com>
2017-10-01 00:07:30 -0700

    Added search field on main page

--- app/src/main/java/com/android/filter/MainActivity.java
+++ app/src/main/java/com/android/filter/MainActivity.java
@@ -1,30 +1,45 @@
 package com.android.filter;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setOnClick(R.id.tsunami, TsunamiActivity.class);
-    }
-
-    private void setOnClick(int resourceID, final Class nextActivity) {
-        TextView textView = (TextView) findViewById(resourceID);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.searchLocation);
+        button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent explicitIntent = new Intent(MainActivity.this, nextActivity);
-                startActivity(explicitIntent);
-                Log.v("ACTIVITY LAUNCHED", String.format("Launched %s", nextActivity));
+                // Code here executes on main thread after user presses button
+                String result = editText.getText().toString();
+
+                if (result.length() < 1) {
+                    AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                    alertDialog.setTitle("Invalid Location");
+                    alertDialog.setMessage("Please enter a valid location.");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog.show();
+                } else {
+                    result = result.toLowerCase();
+                }
             }
         });
     }
