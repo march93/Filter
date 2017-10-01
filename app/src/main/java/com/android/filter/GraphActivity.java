8faf09d62b834d017c667e8d6336804cf72b5ad2
Daniel <dla107@sfu.ca>
2017-10-01 00:24:31 -0700

    Fixed bar width

---
+++ app/src/main/java/com/android/filter/GraphActivity.java
@@ -0,0 +1,59 @@
+package com.android.filter;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.ValueDependentColor;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+
+public class GraphActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
+                //new DataPoint(x,y) parameters for where we want to show the graph
+                new DataPoint(0, -1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
+        /*Drawing bar graph, implement with Gabe's network code*/
+        // styling
+        series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
+            @Override
+            public int get(DataPoint data) {
+                return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100);
+            }
+        });
+
+        //First and Last bar width is halved without this code
+        double xInterval=1.0;
+        graph.getViewport().setXAxisBoundsManual(true);
+        if (series instanceof BarGraphSeries ) {
+            // Hide xLabels for now as no longer centered in the grid, but left aligned per the other types
+            graph.getGridLabelRenderer().setHorizontalLabelsVisible(true);
+            // Shunt the viewport, per v3.1.3 to show the full width of the first and last bars.
+            graph.getViewport().setMinX(series.getLowestValueX() - (xInterval/2.0));
+            graph.getViewport().setMaxX(series.getHighestValueX() + (xInterval/2.0));
+        } else {
+            graph.getViewport().setMinX(series.getLowestValueX() );
+            graph.getViewport().setMaxX(series.getHighestValueX());
+        }
+
+        series.setSpacing(25);
+
+        // draw values on top
+        series.setDrawValuesOnTop(true);
+        series.setValuesOnTopColor(Color.RED);
+        //series.setValuesOnTopSize(50);`
+    }
+}
--- app/src/main/java/com/android/filter/MainActivity.java
+++ app/src/main/java/com/android/filter/MainActivity.java
@@ -26,31 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, -1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-        graph.addSeries(series);
-
-        /*Drawing bar graph, implement with Gabe's network code*/
-        // styling
-        series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
-            @Override
-            public int get(DataPoint data) {
-                return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100);
-            }
-        });
-
-        series.setSpacing(50);
-
-        // draw values on top
-        series.setDrawValuesOnTop(true);
-        series.setValuesOnTopColor(Color.RED);
-        //series.setValuesOnTopSize(50);`
+        setOnClick(R.id.graph, GraphActivity.class);
         setOnClick(R.id.tsunami, TsunamiActivity.class);
     }
 
